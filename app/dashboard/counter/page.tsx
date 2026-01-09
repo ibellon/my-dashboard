f297cda184dc61935473101aa02368f3ddfd5130
ENMAD0386\Isidro Bellón <isidro.bellon@entelgy.com>
2026-01-09 11:53:01 +0100

    Primer commit

---
+++ app/dashboard/counter/page.tsx
@@ -0,0 +1,18 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "Un simple contador"
+}
+
+export default function CounterPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el Carrito de Compras</span>
+
+      <CartCounter value={20}></CartCounter>
+
+    </div>
+  );
+}
